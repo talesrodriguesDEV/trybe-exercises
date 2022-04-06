80d40c2abe7132448967b2cf575e6e62ee7773d4
Tales Rodrigues <tales00001@gmail.com>
2022-04-06 15:35:12 -0300

    adiciona exercício 9

---
+++ fundamentos/bloco4/dia1/ex9.js
@@ -0,0 +1,12 @@
+const a = 2;
+const b = 4;
+const c = 7;
+let aux;
+
+if (a%2 == 0 && b%2 == 0 && c%2 == 0) {
+    aux = false;
+    console.log(aux);
+} else {
+    aux = true;
+    console.log(aux);
+}
