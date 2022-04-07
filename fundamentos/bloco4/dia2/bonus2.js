b40870ced651b9158b60c6231047e540825c2307
Tales Rodrigues <tales00001@gmail.com>
2022-04-07 15:25:10 -0300

    adiciona bônus 2 (bloco 4 dia 2)

---
+++ fundamentos/bloco4/dia2/bonus2.js
@@ -0,0 +1,14 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let aux = 0;
+
+for (let i = 0; i < numbers.length - 1; i += 1) {
+    for (let j = i + 1; j < numbers.length; j += 1) {
+        if (numbers[i] < numbers[j]) {
+            aux = numbers[i];
+            numbers[i] = numbers[j];
+            numbers[j] = aux;
+        }
+    }
+}
+
+console.log(numbers);
