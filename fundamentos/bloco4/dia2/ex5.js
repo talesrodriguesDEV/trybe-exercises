07488497403afa79796cd7c159fbe82da438a73a
Tales Rodrigues <tales00001@gmail.com>
2022-04-07 14:50:43 -0300

    adiciona exercício 5 (bloco 4 dia 2)

--- fundamentos/bloco4/dia2/ex5.js
+++ fundamentos/bloco4/dia2/ex5.js
@@ -2,10 +2,8 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let maior = 0;
 
 for (let i = 0; i < numbers.length; i += 1) {
-    if (numbers[i] > numbers[i + 1]) {
-        maior = numbers[i]
-    } else {
-        maior = numbers[i + 1]
+    if (numbers[i] > maior) {
+        maior = numbers[i];
     }
 }
 
