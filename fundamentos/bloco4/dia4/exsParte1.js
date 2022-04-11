cc2fcd5501cac83513636651aa5d2f970817e0c8
Tales Rodrigues <tales00001@gmail.com>
2022-04-11 15:30:53 -0300

    adiciona exercício 7 parte I (bloco4.4)

--- fundamentos/bloco4/dia4/exsParte1.js
+++ fundamentos/bloco4/dia4/exsParte1.js
@@ -44,3 +44,11 @@ let leitor = {
     ],
 };
 console.log("O livro favorito de " + leitor.nome + " " + leitor.sobrenome + " se chama '" + leitor.livrosFavoritos[0].titulo + "'");
+//7
+leitor.livrosFavoritos.push({
+    titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+    autor: 'JK Rowling',
+    editora: 'Rocco',
+    });
+
+console.table(leitor.livrosFavoritos);
