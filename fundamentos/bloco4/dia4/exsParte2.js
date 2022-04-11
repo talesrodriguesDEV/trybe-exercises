8547909665b80ce89380e6626e3e6ced11877538
Tales Rodrigues <tales00001@gmail.com>
2022-04-11 19:03:31 -0300

    adiciona exercício 2 parte II (bloco4.4)

--- fundamentos/bloco4/dia4/exsParte2.js
+++ fundamentos/bloco4/dia4/exsParte2.js
@@ -1,3 +1,4 @@
+//1
 function palindromeVerifier(string) {
     array = string.split('');
     let palindrome = true;
@@ -18,3 +19,16 @@ function palindromeVerifier(string) {
     }
 }
 console.log(palindromeVerifier('doidera'));;
+//2
+function maior(array) {
+    let maiorNum = 0;
+    let indice = 0;
+    for(let index = 0; index < array.length; index += 1) {
+        if(array[index] > maiorNum) {
+            maiorNum = array[index];
+            indice = index;
+        }
+    }
+    return indice;
+}
+console.log(maior([2, 3, 11, 7, 1, 10]));
