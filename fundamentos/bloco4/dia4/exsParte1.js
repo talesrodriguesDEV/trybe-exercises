400215cc83d83cbf162272d219baaa064c9bde19
Tales Rodrigues <tales00001@gmail.com>
2022-04-11 15:26:16 -0300

    adiciona exercício 6 parte I (bloco4.4)

--- fundamentos/bloco4/dia4/exsParte1.js
+++ fundamentos/bloco4/dia4/exsParte1.js
@@ -30,3 +30,17 @@ for(let key in info) {
         console.log('Ambos recorrentes');
     }
 }
+//6
+let leitor = {
+    nome: 'Julia',
+    sobrenome: 'Pessoa',
+    idade: 21,
+    livrosFavoritos: [
+      {
+        titulo: 'O Pior Dia de Todos',
+        autor: 'Daniela Kopsch',
+        editora: 'Tordesilhas',
+      },
+    ],
+};
+console.log("O livro favorito de " + leitor.nome + " " + leitor.sobrenome + " se chama '" + leitor.livrosFavoritos[0].titulo + "'");
