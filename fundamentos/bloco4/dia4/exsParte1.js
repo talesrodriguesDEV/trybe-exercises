5613b257768b66fc8c749ba661eac1f5ed7fcee1
Tales Rodrigues <tales00001@gmail.com>
2022-04-11 15:21:10 -0300

    adiciona exercício 5 parte I (bloco4.4)

--- fundamentos/bloco4/dia4/exsParte1.js
+++ fundamentos/bloco4/dia4/exsParte1.js
@@ -16,4 +16,17 @@ for(let key in info) {
 for(let key in info) {
     console.log(info[key]);
 }
-
+//5
+let info2 = {
+    personagem: 'Tio Patinhas',
+    origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+    nota: 'O último MacPatinhas',
+    recorrente: 'Sim',
+};
+for(let key in info) {
+    if(info[key] != info2[key]) {
+        console.log(info[key] + ' e ' + info2[key]);
+    } else {
+        console.log('Ambos recorrentes');
+    }
+}
