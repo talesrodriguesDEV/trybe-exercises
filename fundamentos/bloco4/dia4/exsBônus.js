6b4db8796a28c3d81eae73df449135f35202301c
Tales Rodrigues <tales00001@gmail.com>
2022-04-12 10:58:31 -0300

    adiciona exercício 2 bônus  (bloco4.4)

---
+++ fundamentos/bloco4/dia4/exsBônus.js
@@ -0,0 +1,21 @@
+//1
+function numConversor(roman) {
+    let conversor = {
+        I: "1",
+
+    };
+    return indoA;
+}
+//2
+function arrayOfNumbers(vector) {
+    let pares = [];
+    for(let index = 0; index < vector.length; index += 1) {
+        for(let index2 = 0; index2 < vector[index].length; index2 += 1) {
+            if(vector[index][index2] % 2 === 0) {
+                pares.push(vector[index][index2]);
+            }
+        }
+    }
+    return pares;
+}
+console.log(arrayOfNumbers([[1, 2, 11, 13, 14], [3,4,5,21,6,26,31], [7,8,9,40,57, 90, 83, 10]]));
