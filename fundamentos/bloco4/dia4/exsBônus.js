ca8e11b6aebbc5a7266778c994c81c42dbf0e1b3
Tales Rodrigues <tales00001@gmail.com>
2022-04-12 13:09:31 -0300

    adiciona exercício 3 bônus  (bloco4.4)

--- fundamentos/bloco4/dia4/exsBônus.js
+++ fundamentos/bloco4/dia4/exsBônus.js
@@ -19,3 +19,64 @@ function arrayOfNumbers(vector) {
     return pares;
 }
 console.log(arrayOfNumbers([[1, 2, 11, 13, 14], [3,4,5,21,6,26,31], [7,8,9,40,57, 90, 83, 10]]));
+//3
+const basket = [
+    'Melancia', 'Abacate', 'Melancia', 'Melancia', 'Uva', 'Laranja',
+    'Jaca', 'Pera', 'Melancia', 'Uva', 'Laranja', 'Melancia',
+    'Banana', 'Uva', 'Pera', 'Abacate', 'Laranja', 'Abacate',
+    'Banana', 'Melancia', 'Laranja', 'Laranja', 'Jaca', 'Uva',
+    'Banana', 'Uva', 'Laranja', 'Pera', 'Melancia', 'Uva',
+    'Jaca', 'Banana', 'Pera', 'Abacate', 'Melancia', 'Melancia',
+    'Laranja', 'Pera', 'Banana', 'Jaca', 'Laranja', 'Melancia',
+    'Abacate', 'Abacate', 'Pera', 'Melancia', 'Banana', 'Banana',
+    'Abacate', 'Uva', 'Laranja', 'Banana', 'Abacate', 'Uva',
+    'Uva', 'Abacate', 'Abacate', 'Melancia', 'Uva', 'Jaca',
+    'Uva', 'Banana', 'Abacate', 'Banana', 'Uva', 'Banana',
+    'Laranja', 'Laranja', 'Jaca', 'Jaca', 'Abacate', 'Jaca',
+    'Laranja', 'Melancia', 'Pera', 'Jaca', 'Melancia', 'Uva',
+    'Abacate', 'Jaca', 'Jaca', 'Abacate', 'Uva', 'Laranja',
+    'Pera', 'Melancia', 'Jaca', 'Pera', 'Laranja', 'Jaca',
+    'Pera', 'Melancia', 'Jaca', 'Banana', 'Laranja', 'Jaca',
+    'Banana', 'Pera', 'Abacate', 'Uva',
+];
+function fruitsPicking(basket) {
+    let fruits = {
+        melancia: 0,
+        abacate: 0,
+        uva: 0,
+        laranja: 0,
+        jaca: 0,
+        pera: 0,
+        banana: 0,
+    }
+    for(let index = 0; index < basket.length; index += 1) {
+        let fruta = basket[index];
+        switch(fruta) {
+            case 'Melancia':
+                fruits.melancia += 1;
+                break;
+            case 'Abacate':
+                fruits.abacate += 1;
+                break;
+            case 'Uva':
+                fruits.uva += 1;
+                break;   
+            case 'Laranja':
+                fruits.laranja += 1;
+                break; 
+            case 'Jaca':
+                fruits.jaca += 1;
+                break; 
+            case 'Pera':
+                fruits.pera += 1;
+                break; 
+            case 'Banana':
+                fruits.banana += 1;
+                break;                  
+        }
+    }
+    //return fruits;
+    return "Sua cesta possui " + fruits.melancia + " melâncias, " + fruits.abacate + " abacates, " + fruits.uva + " uvas, " + fruits.laranja + " laranjas, " + fruits.jaca + " jacas, " + fruits.pera + " pêras e " + fruits.banana + " bananas.";
+}
+console.log(fruitsPicking(basket));
+//4
