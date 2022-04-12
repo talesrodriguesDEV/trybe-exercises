54ed8cf4396d699502fb5e47243b1c14e071f4fa
Tales Rodrigues <tales00001@gmail.com>
2022-04-12 13:17:25 -0300

    adiciona exercício 4 bônus  (bloco4.4)

--- fundamentos/bloco4/dia4/exsBônus.js
+++ fundamentos/bloco4/dia4/exsBônus.js
@@ -80,3 +80,35 @@ function fruitsPicking(basket) {
 }
 console.log(fruitsPicking(basket));
 //4
+let moradores = {
+    blocoUm: [
+      {
+        nome: 'Luiza',
+        sobrenome: 'Guimarães',
+        andar: 10,
+        apartamento: 1005,
+      },
+      {
+        nome: 'William',
+        sobrenome: 'Albuquerque',
+        andar: 5,
+        apartamento: 502,
+      },
+    ],
+    blocoDois: [
+      {
+        nome: 'Murilo',
+        sobrenome: 'Ferraz',
+        andar: 8,
+        apartamento: 804,
+      },
+      {
+        nome: 'Zoey',
+        sobrenome: 'Brooks',
+        andar: 1,
+        apartamento: 101,
+      },
+    ],
+  };
+console.log("A pessoa moradora do bloco 2 de nome " + moradores.blocoDois[1].nome + " " + moradores.blocoDois[1].sobrenome + " mora no " + moradores.blocoDois[1].andar + "º andar, apartamento " + moradores.blocoDois[1].apartamento);
+//5
