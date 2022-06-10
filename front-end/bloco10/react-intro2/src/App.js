0d8d16611112363e07363b03b4de540bd7e3d5d3
Tales Rodrigues <tales00001@gmail.com>
2022-06-10 14:51:37 -0300

    Introduction to React

---
+++ front-end/bloco10/react-intro2/src/App.js
@@ -0,0 +1,13 @@
+import Header from './Header';
+import Content from './Content';
+import Footer from './Footer';
+
+function App() {
+  return (
+    <Header/>,
+    <Content/>,
+    <Footer/>
+  );
+}
+
+export default App;
